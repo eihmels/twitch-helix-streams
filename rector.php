6d20cbd8c17e4022a1155fbafe7c6acd1dbfe28e
Edzard Ihmels <edzard.ihmels@check24.de>
2023-02-02 21:35:43 +0100

    rectorChanges

---
+++ rector.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
+use Rector\Config\RectorConfig;
+use Rector\Doctrine\Set\DoctrineSetList;
+use Rector\PHPUnit\Set\PHPUnitLevelSetList;
+use Rector\Privatization\Rector\Class_\FinalizeClassesWithoutChildrenRector;
+use Rector\Set\ValueObject\SetList;
+use Rector\TypeDeclaration\Rector\ClassMethod\ReturnTypeFromStrictTypedPropertyRector;
+use Rector\TypeDeclaration\Rector\Param\ParamTypeFromStrictTypedPropertyRector;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->paths([
+        __DIR__ . '/src',
+        __DIR__ . '/tests'
+    ]);
+
+    $rectorConfig->importShortClasses();
+    $rectorConfig->importNames();
+
+
+    // register a single rule
+    $rectorConfig->rule(InlineConstructorDefaultToPropertyRector::class);
+    $rectorConfig->rule(ParamTypeFromStrictTypedPropertyRector::class);
+    $rectorConfig->rule(ReturnTypeFromStrictTypedPropertyRector::class);
+
+    // define sets of rules
+    $rectorConfig->sets([
+        PHPUnitLevelSetList::UP_TO_PHPUNIT_90,
+        DoctrineSetList::DOCTRINE_CODE_QUALITY,
+        SetList::CODING_STYLE,
+        SetList::CODE_QUALITY,
+        SetList::PHP_80,
+        SetList::NAMING,
+        SetList::EARLY_RETURN,
+        SetList::TYPE_DECLARATION
+    ]);
+};
